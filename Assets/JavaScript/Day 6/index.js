8e2e8e0bc88835e30ed9c2b3a1c085c8d44c88d3
ShelbyDev-er <104688251+ShelbyDev-er@users.noreply.github.com>
2022-09-15 14:22:13 +0700

    update code

---
+++ Assets/JavaScript/Day 6/index.js
@@ -0,0 +1,42 @@
+const myClass = [
+    {
+        name: 'Nguyen Van A',
+        phoneNumber: '0912345623'
+    },
+    {
+        name: 'Nguyen Van ADSGEG',
+        phoneNumber: '0912325622'
+    },
+    {
+        name: 'Nguyen Van ADSGE',
+        phoneNumber: '0912325622'
+    },
+    {
+        name: 'Nguyen Van ADSG',
+        phoneNumber: '0912245622'
+    },
+    {
+        name: 'Nguyen Van ADS',
+        phoneNumber: '0912345623'
+    },
+    {
+        name: 'Nguyen Van ADDAW',
+        phoneNumber: '0912325623'
+    },
+    {
+        name: 'Nguyen Van ADDA',
+        phoneNumber: '0912342623'
+    },
+    {
+        name: 'Nguyen Van ADD',
+        phoneNumber: '0912345223'
+    }
+]
+
+myClass[1].name = 'Nguyen Van B'
+myClass[2].name = 'Nguyen Van C'
+
+myClass.push({
+    name: 'Nguyen Van Z',
+    phoneNumber: '0988888888'
+})
